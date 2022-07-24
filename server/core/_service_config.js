3c9f16e719d8f5ed584d4c07e36da55f635aea3f
Eduardo Velasques <eduveks@gmail.com>
2022-07-24 14:52:46 +0000

    Allow public access to services.

--- server/core/_service_config.js
+++ server/core/_service_config.js
@@ -11,3 +11,12 @@ if (_service.path == 'samples/my-service') {
     _service.allow()
 }
 */
+
+if (_service.path == 'people/post'
+   || _service.path == 'people/options'
+   || _service.path == 'recovery/put'
+   || _service.path == 'recovery/post'
+   || _service.path == 'recovery/options') {
+    _service.allow()
+}
+
