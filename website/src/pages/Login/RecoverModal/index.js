c803b73066eaf6cedf1e879e420c4b60d103529b
Eduardo Velasques <eduveks@gmail.com>
2022-07-25 23:22:30 +0000

    Login fixes.

--- website/src/pages/Login/RecoverModal/index.js
+++ website/src/pages/Login/RecoverModal/index.js
@@ -9,98 +9,97 @@ import './index.less';
 
 export default function RecoverModal(props) {
 
-    const [submitting, setSubmitting] = useState(false);
-    const [visible, setVisible] = useState(true);
-    const recoverForm = useRef(null);
+  const [submitting, setSubmitting] = useState(false);
+  const [visible, setVisible] = useState(true);
+  const recoverForm = useRef(null);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    });
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
-    function onFinish(values) {
-        setSubmitting(true);
-        const { mail } = values;
-        _service({
-            method: 'POST',
-            url: 'recovery',
-            data: {
-                mail,
-            },
-            success: (response) => {
-                if (response.json.result) {
-                    notification["success"]({
-                        message: 'Alteração da Palavra-Passe ',
-                        description: 'Foi enviado um e-mail para a alteração da Palavra-Passe.',
-                    });
-                    setSubmitting(false);
-                    setVisible(false);
-                }
-            },
-            fail: () => {
-                setSubmitting(false);
-                notification["error"]({
-                    message: 'Erro na Alteração da Palavra-Passe',
-                    description: 'Não foi possível alterar a palavra-passe, contacte-nos através do chat de suporte.',
-                });
-            }
-        });
-    }
-
-    function onFinishFailed(errorInfo) {
-        console.log('Failed:', errorInfo);
-    }
-
-    function onSubmit() {
-        recoverForm.current.validateFields()
-            .then(values => {
-                recoverForm.current.resetFields();
-                onFinish(values);
-            })
-            .catch(info => {
-                console.log('Validate Failed:', info);
-            });
-    }
-
-    function onCancel() {
-        setVisible(false);
-        if (props.onClose) {
-            props.onClose();
+  function onFinish(values) {
+    setSubmitting(true);
+    const { mail } = values;
+    _service({
+      method: 'POST',
+      url: 'recovery',
+      data: {
+        mail,
+      },
+      success: (response) => {
+        if (response.json.result) {
+          notification["success"]({
+            message: 'Alteração da Palavra-Passe ',
+            description: 'Foi enviado um e-mail para a alteração da Palavra-Passe.',
+          });
+          setSubmitting(false);
+          setVisible(false);
         }
+      },
+      fail: () => {
+        setSubmitting(false);
+        notification["error"]({
+          message: 'Erro na Alteração da Palavra-Passe',
+          description: 'Não foi possível alterar a palavra-passe, contacte-nos através do chat de suporte.',
+        });
+      }
+    });
+  }
+
+  function onFinishFailed(errorInfo) {
+    console.log('Failed:', errorInfo);
+  }
+
+  function onSubmit() {
+    recoverForm.current.validateFields()
+      .then(values => {
+        recoverForm.current.resetFields();
+        onFinish(values);
+      })
+      .catch(info => {
+        console.log('Validate Failed:', info);
+      });
+  }
+
+  function onCancel() {
+    setVisible(false);
+    if (props.onClose) {
+      props.onClose();
     }
+  }
 
-    return (
-        <Modal
-            className={'modal-recover'}
-            title="Recuperar o Acesso"
-            visible={visible}
-            onCancel={onCancel}
-            footer={[
-                <Button key="back" onClick={onCancel}>
-                    Cancelar
-                </Button>,
-                <Button key="send" type="primary" htmlType="submit" loading={submitting} onClick={onSubmit} >
-                    Enviar
-                </Button>
-            ]}
+  return (
+    <Modal
+      className={'modal-recover'}
+      title="Recuperar o Acesso"
+      visible={visible}
+      onCancel={onCancel}
+      footer={[
+        <Button key="back" onClick={onCancel}>
+          Cancelar
+        </Button>,
+        <Button key="send" type="primary" htmlType="submit" loading={submitting} onClick={onSubmit} >
+          Enviar
+        </Button>
+      ]}
+    >
+      <Form
+        ref={recoverForm}
+        name="basic"
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Endereço de Mail"
+          name="mail"
+          rules={[
+            { type: 'email', message: 'O e-mail inserido não é válido.' },
+            { required: true, message: 'Insira o e-mail.' }
+          ]}
         >
-            <Form
-                ref={recoverForm}
-                name="basic"
-                onFinishFailed={onFinishFailed}
-            >
-                <Form.Item
-                    label="Endereço de Mail"
-                    name="mail"
-                    rules={[
-                        { type: 'email', message: 'O e-mail inserido não é válido.' },
-                        { required: true, message: 'Insira o e-mail.' }
-                    ]}
-                >
-                    <Input disabled={submitting} maxLength={250} />
-                </Form.Item>
-            </Form>
+          <Input disabled={submitting} maxLength={250} />
+        </Form.Item>
+      </Form>
 
-        </Modal>
-    );
-
+    </Modal>
+  );
 }
--- website/src/pages/Login/index.js
+++ website/src/pages/Login/index.js
@@ -11,116 +11,114 @@ const { Content, Sider } = Layout;
 
 export default function Login(props) {
 
-    const [submitting, setSubmitting] = useState(false);
-    const [visible, setVisible] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    useEffect(() => {
-        if (_auth.isLogged()) {
-            window.scrollTo(0, 0);
-        }
-        window.scrollTo(0, 0);
-    });
-
-    function onFinish(values) {
-        setSubmitting(true);
-        const { username, password, remember } = values;
-        if (remember) {
-            localStorage.setItem("login", JSON.stringify(values));
-        } else {
-            localStorage.removeItem("login");
-        }
-        _auth.login({
-            username,
-            password,
-            success: () => {
-                setSubmitting(false);
-            },
-            fail: () => {
-                setSubmitting(false);
-                notification["error"]({
-                    message: 'Login Inválido',
-                    description:
-                        'Por favor verifique as credenciais inseridas.',
-                });
-            }
-        });
-    }
-
-    function onFinishFailed(errorInfo) {
-        console.log('Failed:', errorInfo);
-    }
-
-    let initialValues = { remember: true };
-    if (localStorage.getItem("login") != null) {
-        initialValues = JSON.parse(localStorage.getItem("login"));
-    }
-
+  useEffect(() => {
     if (_auth.isLogged()) {
-        window.setTimeout(() => window.location.reload(), 250);
-        return <Navigate to="/reserved-area" />;
-    } else {
-        return (
-            <Layout>
-                <Content className="login-container">
-                    <div className="content-title">
-                        <Title>Iniciar sessão.</Title>
-                    </div>
-                    <div className="content-body">
-                        <p>Inicie sessão com os seus dados.</p>
-                        <Form
-                            layout="vertical"
-                            name="basic"
-                            initialValues={initialValues}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
-                        >
-                            <Form.Item
-                                label="Utilizador"
-                                name="username"
-                                rules={[{ required: true, message: 'Insira o seu utilizador.' }]}
-                            >
-                                <Input loading={submitting} />
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Palavra-passe"
-                                name="password"
-                                rules={[{ required: true, message: 'Insira a palavra-passe.' }]}
-                            >
-                                <Input.Password loading={submitting} />
-                            </Form.Item>
-
-                            <Form.Item name="remember" valuePropName="checked">
-                                <Checkbox>Relembrar</Checkbox>
-                            </Form.Item>
-
-                            <Form.Item>
-                                <Button loading={submitting} type="primary" className="login-btn" htmlType="submit">
-                                    Iniciar Sessão
-                                </Button>
-                            </Form.Item>
-
-                            <Form.Item style={{ textAlign: 'center' }}>
-                                <Button type="link" onClick={() => setVisible(!visible)} >Esqueceu-se da palavra passe?</Button>
-                                {visible && <RecoverModal onClose={() => { setVisible(false) }} />}
-                            </Form.Item>
-
-                            <hr />
-                            <span><p>ou</p></span>
-                            <Link to="/register">
-                                <Button loading={submitting} type="default" className={"register-btn"}>
-                                    Criar Conta
-                            </Button>
-                            </Link>
-                        </Form>
-                    </div>
-
-                </Content>
-                <Sider width={'50%'}>
-                    <span class="helper" /><img alt="sider-login" src={"/images/sider-login.png"} />
-                </Sider>
-            </Layout>
-        );
+      window.scrollTo(0, 0);
     }
+    window.scrollTo(0, 0);
+  });
 
+  function onFinish(values) {
+    setSubmitting(true);
+    const { username, password, remember } = values;
+    if (remember) {
+      localStorage.setItem("login", JSON.stringify(values));
+    } else {
+      localStorage.removeItem("login");
+    }
+    _auth.login({
+      username,
+      password,
+      success: () => {
+        setSubmitting(false);
+      },
+      fail: () => {
+        setSubmitting(false);
+        notification["error"]({
+          message: 'Login Inválido',
+          description:
+          'Por favor verifique as credenciais inseridas.',
+        });
+      }
+    });
+  }
+
+  function onFinishFailed(errorInfo) {
+    console.log('Failed:', errorInfo);
+  }
+
+  let initialValues = { remember: true };
+  if (localStorage.getItem("login") != null) {
+    initialValues = JSON.parse(localStorage.getItem("login"));
+  }
+
+  if (_auth.isLogged()) {
+    return <Navigate to="/reserved-area" />;
+  } else {
+    return (
+      <Layout>
+        <Content className="login-container">
+          <div className="content-title">
+            <Title>Iniciar sessão.</Title>
+          </div>
+          <div className="content-body">
+            <p>Inicie sessão com os seus dados.</p>
+            <Form
+              layout="vertical"
+              name="basic"
+              initialValues={initialValues}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+            >
+              <Form.Item
+                label="Utilizador"
+                name="username"
+                rules={[{ required: true, message: 'Insira o seu utilizador.' }]}
+              >
+                <Input loading={submitting} />
+              </Form.Item>
+
+              <Form.Item
+                label="Palavra-passe"
+                name="password"
+                rules={[{ required: true, message: 'Insira a palavra-passe.' }]}
+              >
+                <Input.Password loading={submitting} />
+              </Form.Item>
+
+              <Form.Item name="remember" valuePropName="checked">
+                <Checkbox>Relembrar</Checkbox>
+              </Form.Item>
+
+              <Form.Item>
+                <Button loading={submitting} type="primary" className="login-btn" htmlType="submit">
+                  Iniciar Sessão
+                </Button>
+              </Form.Item>
+
+              <Form.Item style={{ textAlign: 'center' }}>
+                <Button type="link" onClick={() => setVisible(!visible)} >Esqueceu-se da palavra passe?</Button>
+                {visible && <RecoverModal onClose={() => { setVisible(false) }} />}
+              </Form.Item>
+
+              <hr />
+              <span><p>ou</p></span>
+              <Link to="/register">
+                <Button loading={submitting} type="default" className={"register-btn"}>
+                  Criar Conta
+                </Button>
+              </Link>
+            </Form>
+          </div>
+
+        </Content>
+        <Sider width={'50%'}>
+          <span class="helper" /><img alt="sider-login" src={"/images/sider-login.png"} />
+        </Sider>
+      </Layout>
+    );
+  }
 }
