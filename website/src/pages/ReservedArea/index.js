7146d756a77e0e0979409f0fefc9f7796a3986af
Eduardo Velasques <eduveks@gmail.com>
2022-12-27 14:25:05 +0000

    The ReservedArea page uses user info from the redux state.

--- website/src/pages/ReservedArea/index.js
+++ website/src/pages/ReservedArea/index.js
@@ -1,22 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { Navigate } from "react-router-dom";
 
-import { Typography } from 'antd';
+import { Typography, Spin } from 'antd';
 
-import _service from '@netuno/service-client';
+import { connect } from 'react-redux';
+
 import _auth from '@netuno/auth-client';
 
 import './index.less';
 
 const { Title } = Typography;
 
-function ReservedArea({userInfoAction}) {
+function ReservedArea({loggedUserInfo}) {
   if (_auth.isLogged()) {
+    let content = null;
+    if (!loggedUserInfo) {
+      content = <Spin/>;
+    } else {
+      content = (
+        <>
+          <Title level={2}>Olá {loggedUserInfo.name}!</Title>
+          <Title level={3} style={{ marginTop: 0 }}>Bem-vindo(a) à sua Área Reservada!</Title>
+          <img alt="reserved-area" src={"/images/reserved-area.png"} />
+        </>
+      );
+    }
     return (
       <div className="dashboard-layout-content">
-        <Title level={2}>Olá!</Title>
-        <Title level={3} style={{ marginTop: 0 }}>Bem-vindo(a) à sua Área Reservada!</Title>
-        <img alt="reserved-area" src={"/images/reserved-area.png"} />
+        {content}
       </div>
     );
   } else {
@@ -24,4 +35,12 @@ function ReservedArea({userInfoAction}) {
   }
 }
 
-export default ReservedArea;
+const mapStateToProps = store => {
+  const { loggedUserInfo } = store.loggedUserInfoState;
+  return {
+    loggedUserInfo
+  };
+};
+
+export default connect(mapStateToProps, {})(ReservedArea);
+
