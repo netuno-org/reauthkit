dc107f8dc3cbeee00ead02f9adec93dff9363b64
Eduardo Velasques <eduveks@gmail.com>
2023-10-25 09:36:48 -0300

    Register page with authentication providers buttons.

--- website/src/pages/Register/index.jsx
+++ website/src/pages/Register/index.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { Layout, Typography, Form, Input, Button, notification } from 'antd';
 import { PasswordInput } from "antd-password-input-strength";
 import _auth from '@netuno/auth-client';
 import _service from '@netuno/service-client';
+
+import {
+  FaFacebook, FaGoogle, FaDiscord, FaGithub
+} from "react-icons/fa";
+
+import Config from '../../common/Config';
 
 import './index.less';
 
@@ -11,17 +17,18 @@ const { Title } = Typography;
 const { Content, Sider } = Layout;
 
 export default function Register(props) {
-
+  const servicePrefix = _service.config().prefix;
   const [ready, setReady] = useState(false);
   const [submitting, setSubmitting] = useState(false);
   const registerForm = useRef(null);
+  const { provider } = useParams(null);
 
   useEffect(() => {
     if (_auth.isLogged()) {
       window.scrollTo(0, 0);
     }
     window.scrollTo(0, 0);
-  });
+  }, []);
 
   function onFinish(values) {
     setSubmitting(true);
@@ -76,95 +83,110 @@ export default function Register(props) {
   if (_auth.isLogged()) {
     return <Navigate to="/reserved-area" />;
   }
-  else if (ready) {
+  if (ready) {
     return <Navigate to="/login" />;
-  } else {
-    return (
-      <Layout>
-        <Content className="register-container">
-          <div className="content-title">
-            <Title>Criar conta.</Title>
-          </div>
-          <div className="content-body">
-            <p>Crie uma conta para poder aceder à sua área reservada.</p>
-            <Form
-              ref={registerForm}
-              layout="vertical"
-              name="basic"
-              initialValues={{ remember: true }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-            >
-              <Form.Item
-                label="Nome"
-                name="name"
-                rules={[
-                  { required: true, message: 'Insira o seu nome.' },
-                  { type: 'string', message: 'Nome inválido, apenas letras minúsculas e maiúsculas.', pattern: "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$" }
-                ]}
-              >
-                <Input disabled={submitting} maxLength={25} />
-              </Form.Item>
-              <Form.Item
-                label="Utilizador"
-                name="username"
-                rules={[
-                  { required: true, message: 'Insira o seu usuário.' },
-                  { type: 'string', message: 'Usuário inválido, apenas letras minúsculas e maiúsculas.', pattern: "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$" }
-                ]}
-              >
-                <Input disabled={submitting} maxLength={25} />
-              </Form.Item>
-              <Form.Item
-                label="E-mail"
-                name="email"
-                rules={[
-                  { type: 'email', message: 'O e-mail inserido não é válido.' },
-                  { required: true, message: 'Insira o e-mail.' }
-                ]}
-              >
-                <Input disabled={submitting} maxLength={250} />
-              </Form.Item>
-              <Form.Item
-                label="Palavra-passe"
-                name="password"
-                rules={[
-                  { required: true, message: 'Insira a palavra-passe.' },
-                  { type: 'string', message: 'Palavra-Passe deverá ter entre 8 a 25 caracteres.', min: 8, max: 25 },
-                ]}
-              >
-                <PasswordInput disabled={submitting} maxLength={25} />
-              </Form.Item>
-              <Form.Item
-                label="Confirmar a Palavra-passe"
-                name="password_confirm"
-                rules={[
-                  { required: true, message: 'Insira a confirmação da palavra-passe.' },
-                  { type: 'string', message: 'Palavra-Passe deverá ter entre 8 a 25 caracteres.', min: 8, max: 25 },
-                  ({ getFieldValue }) => ({
-                    validator(_, value) {
-                      if (!value || getFieldValue('password') === value) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject('As palavras-passes não são iguais.');
-                    },
-                  })
-                ]}
-              >
-                <Input.Password disabled={submitting} maxLength={25} />
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit" loading={submitting}>
-                  Criar Conta
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
-        </Content>
-        <Sider width={'50%'}>
-          <span className="helper" /><img alt="sider-register" src={"/images/sider-register.png"} />
-        </Sider>
-      </Layout>
-    );
   }
+  return (
+    <Layout>
+      <Content className="register-container">
+        <div className="content-title">
+          <Title>Criar conta.</Title>
+        </div>
+        <div className="content-body">
+          <p>Crie uma conta para poder aceder à sua área reservada.</p>
+          <Form
+            ref={registerForm}
+            layout="vertical"
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            { Config.authProviders().facebook &&
+              <Form.Item>
+                <Button href={`${servicePrefix}/_auth_provider/register/facebook`} icon={<FaFacebook />}>Registrar com o Facebook</Button>
+              </Form.Item> }
+            { Config.authProviders().google &&
+              <Form.Item>
+                <Button href={`${servicePrefix}/_auth_provider/register/google`} icon={<FaGoogle />}>Registrar com o Google</Button>
+              </Form.Item> }
+            { Config.authProviders().github &&
+              <Form.Item>
+                <Button href={`${servicePrefix}/_auth_provider/register/github`} icon={<FaGithub />}>Registrar com o GitHub</Button>
+              </Form.Item> }
+            { Config.authProviders().discord &&
+              <Form.Item>
+                <Button href={`${servicePrefix}/_auth_provider/register/discord`} icon={<FaDiscord />}>Registrar com o Discord</Button>
+              </Form.Item> }
+            <Form.Item
+              label="Nome"
+              name="name"
+              rules={[
+                { required: true, message: 'Insira o seu nome.' },
+                { type: 'string', message: 'Nome inválido, apenas letras minúsculas e maiúsculas.', pattern: "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$" }
+              ]}
+            >
+              <Input disabled={submitting} maxLength={250} />
+            </Form.Item>
+            <Form.Item
+              label="Utilizador"
+              name="username"
+              rules={[
+                { required: true, message: 'Insira o seu usuário.' },
+                { type: 'string', message: 'Usuário inválido, apenas letras minúsculas e maiúsculas.', pattern: "^[a-z]+[a-z0-9]{1,24}$" }
+              ]}
+            >
+              <Input disabled={submitting} maxLength={25} />
+            </Form.Item>
+            <Form.Item
+              label="E-mail"
+              name="email"
+              rules={[
+                { type: 'email', message: 'O e-mail inserido não é válido.' },
+                { required: true, message: 'Insira o e-mail.' }
+              ]}
+            >
+              <Input disabled={submitting} maxLength={250} />
+            </Form.Item>
+            <Form.Item
+              label="Palavra-passe"
+              name="password"
+              rules={[
+                { required: true, message: 'Insira a palavra-passe.' },
+                { type: 'string', message: 'Palavra-Passe deverá ter entre 8 a 25 caracteres.', min: 8, max: 25 },
+              ]}
+            >
+              <PasswordInput disabled={submitting} maxLength={25} />
+            </Form.Item>
+            <Form.Item
+              label="Confirmar a Palavra-passe"
+              name="password_confirm"
+              rules={[
+                { required: true, message: 'Insira a confirmação da palavra-passe.' },
+                { type: 'string', message: 'Palavra-Passe deverá ter entre 8 a 25 caracteres.', min: 8, max: 25 },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject('As palavras-passes não são iguais.');
+                  },
+                })
+              ]}
+            >
+              <Input.Password disabled={submitting} maxLength={25} />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={submitting}>
+                Criar Conta
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Content>
+      <Sider width={'50%'}>
+        <span className="helper" /><img alt="sider-register" src={"/images/sider-register.png"} />
+      </Sider>
+    </Layout>
+  );
 }
