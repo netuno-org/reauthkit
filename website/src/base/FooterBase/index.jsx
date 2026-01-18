6d61b303cb109297b5a50b558fde11432fad9c9e
Eduardo Velasques <eduveks@gmail.com>
2026-01-18 20:54:19 +0000

    Website Footer Base component created.

---
+++ website/src/base/FooterBase/index.jsx
@@ -0,0 +1,16 @@
+import _auth from "@netuno/auth-client";
+import {Layout} from "antd";
+
+const {Footer} = Layout;
+
+function FooterBase() {
+    return (
+        <>
+            {!_auth.isLogged() &&
+                <Footer>© netuno.org {new Date().getFullYear()}</Footer>
+            }
+        </>
+    );
+}
+
+export default FooterBase;
