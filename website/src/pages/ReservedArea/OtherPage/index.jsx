2e9fb8e25a7f7c67170cdcec1929d880b19979b3
Eduardo Velasques <eduveks@gmail.com>
2026-01-19 15:17:07 +0000

    Website Reserved Area with other page as example.

---
+++ website/src/pages/ReservedArea/OtherPage/index.jsx
@@ -0,0 +1,18 @@
+import {Typography} from "antd";
+
+import "./index.less";
+
+const { Title } = Typography;
+
+function OtherPage() {
+    return (
+        <section class={"other-page"}>
+            <Title level={1}>Outra Página</Title>
+            <div>
+                <p>Exemplo de uma outra página na área reservada.</p>
+            </div>
+        </section>
+    );
+}
+
+export default OtherPage;
