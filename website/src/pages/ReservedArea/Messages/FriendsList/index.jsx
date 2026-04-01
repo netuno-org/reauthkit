0ac8079d5b92a40d658090c719e8b3effc724d17
Eduardo Velasques <eduveks@gmail.com>
2026-04-01 12:21:01 +0100

    Friend list implemented.

--- website/src/pages/ReservedArea/Messages/FriendsList/index.jsx
+++ website/src/pages/ReservedArea/Messages/FriendsList/index.jsx
@@ -1,8 +1,46 @@
+import {useEffect, useState} from "react";
+import {Spin} from "antd";
+import _ws from "@netuno/ws-client";
 
-function FriendsList() {
+import FriendItem from "./FriendItem";
+
+import "./index.less";
+
+function FriendsList({onFriendSelected}) {
+    const [loading, setLoading] = useState(true);
+    const [list, setList] = useState(null);
+    useEffect(() => {
+        const listenerRef = _ws.addListener({
+            service: "friend/list",
+            success: (data) => {
+                setList(data.content);
+                setLoading(false);
+            },
+            fail: (error)=> {
+                setLoading(false);
+            }
+        });
+        _ws.sendService({
+            service: "friend/list"
+        });
+        return () => {
+            _ws.removeListener(listenerRef);
+        }
+    }, []);
     return (
         <div className="messages__friends-list">
-
+            {loading && <Spin />}
+            {list && <ul>
+                {list.map(({uid, name, avatar}) => (
+                    <FriendItem
+                        key={uid}
+                        uid={uid}
+                        name={name}
+                        avatar={avatar}
+                        onClick={() => onFriendSelected && onFriendSelected({uid, name, avatar})}
+                    />
+                ))}
+            </ul>}
         </div>
     );
 }
