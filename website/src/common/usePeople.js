ea292001a2d03afddb8c7f1b7876504b90d6c89a
Eduardo Velasques <eduveks@gmail.com>
2026-05-02 11:28:26 +0100

    The people hook created with Redux integration.

---
+++ website/src/common/usePeople.js
@@ -0,0 +1,51 @@
+import {peopleLoadAction} from "../redux/actions/index.js";
+import {useDispatch, useSelector} from "react-redux";
+import _service from "@netuno/service-client";
+import globalNotification from "./globalNotification.js";
+import _auth from "@netuno/auth-client";
+
+function usePeople() {
+    const dispatch = useDispatch();
+    const data = useSelector((state) => state.people.data);
+    const load = (onFinish) => {
+        _service({
+            method: 'GET',
+            url: 'people',
+            success: (response) => {
+                if (response.json.result) {
+                    dispatch(peopleLoadAction(response.json.data));
+                    onFinish && onFinish(true);
+                } else {
+                    globalNotification.warning({
+                        title: 'Dados do Utilizador',
+                        description: response.json.error,
+                    });
+                    onFinish && onFinish(false);
+                    _auth.logout();
+                }
+            },
+            fail: (e) => {
+                console.error('Dados do Utilizador', e);
+                globalNotification.serviceFail({
+                    title: 'Dados do Utilizador',
+                    description: 'Ocorreu um erro a carregar os dados, por favor tente novamente mais tarde.',
+                });
+                _auth.logout();
+                onFinish && onFinish(false);
+            }
+        });
+    };
+    return {
+        data,
+        load,
+        unload: () => {
+            dispatch(peopleLoadAction(null));
+        },
+        reload: () => {
+            dispatch(peopleLoadAction(null));
+            load();
+        }
+    };
+}
+
+export default usePeople;
