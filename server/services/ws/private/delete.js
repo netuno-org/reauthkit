7f28c94a7c0bccaf3b1f710d5f50dff28855b8d3
Eduardo Velasques <eduveks@gmail.com>
2026-06-09 17:53:21 +0100

    WS connection notifies friends of online status.

--- server/services/ws/private/delete.js
+++ server/services/ws/private/delete.js
@@ -1,5 +1,12 @@
 import { _db, _ws } from "@netuno/server-types";
 
+import profile from "#core/lib/profile.js";
+import friend from "#core/lib/friend.js";
+
+const dbProfile = profile.getLogged();
+
 _db.form("profile_ws_session")
   .where(_db.where("session_id").equal(_ws.sessionId()))
   .delete();
+
+friend.notifyAllWithStatusChanged(dbProfile, false)
--- server/services/ws/private/post.js
+++ server/services/ws/private/post.js
@@ -1,6 +1,7 @@
 import { _db, _ws } from "@netuno/server-types";
 
 import profile from "#core/lib/profile.js";
+import friend from "#core/lib/friend.js";
 
 const dbProfile = profile.getLogged();
 
@@ -8,3 +9,5 @@ _db.form("profile_ws_session")
   .set("profile_id", dbProfile.getInt("id"))
   .set("session_id", _ws.sessionId())
   .insert();
+
+friend.notifyAllWithStatusChanged(dbProfile, true)
