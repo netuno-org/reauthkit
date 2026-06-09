6c0a439f6f3d143fee3392a74712521c47e8fa95
Eduardo Velasques <eduveks@gmail.com>
2026-06-09 12:27:19 +0100

    WS hook refactored.

--- website/src/common/useWS.js
+++ website/src/common/useWS.js
@@ -3,10 +3,16 @@ import {useDispatch, useSelector} from "react-redux";
 import _ws from "@netuno/ws-client";
 import _auth from "@netuno/auth-client";
 import Config from "./Config.js";
+import {useEffect, useState} from "react";
 
 function useWS() {
   const dispatch = useDispatch();
-  const data = useSelector((state) => state.ws.data);
+  const baseData = useSelector((state) => state.ws.data);
+  const [data, setData] = useState(baseData);
+  const [connecting, setConnecting] = useState(false);
+  useEffect(()=>{
+    setData(baseData);
+  }, [baseData]);
   const load = (onFinish) => {
     _ws.config({
       url: Config.websocketURL() + '?auth='+ _auth.accessToken(),
@@ -15,16 +21,19 @@ function useWS() {
       autoReconnect: true,
       connect: (event) => {
         console.log('ws connect', event);
+        setConnecting(false);
         dispatch(wsLoadAction({connected: true}));
         onFinish && onFinish(true);
       },
       close: (event) => {
         console.log('ws close', event);
+        setConnecting(false);
         dispatch(wsLoadAction({connected: false}));
         onFinish && onFinish(false);
       },
       error: (error) => {
         console.log('ws error', error);
+        setConnecting(false);
         dispatch(wsLoadAction({connected: false}));
         onFinish && onFinish(false);
       },
@@ -33,10 +42,13 @@ function useWS() {
       }
     });
     _ws.connect();
+    setConnecting(true);
   };
   return {
+    isConnecting: ()=> connecting,
     data,
-    load
+    load,
+    close: ()=> _ws.close()
   }
 }
 
