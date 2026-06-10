572f439244eb3ed047d342eb0bbc06224e497672
Eduardo Velasques <eduveks@gmail.com>
2026-06-10 21:02:34 +0100

    Message post service was optimized.

--- server/services/message/post.js
+++ server/services/message/post.js
@@ -1,33 +1,36 @@
 import {_db, _val, _out, _req} from "@netuno/server-types";
 
 import profile from "#core/lib/profile.js";
+import message from "#core/lib/message.js";
 
 const dbProfileFrom = profile.getLogged();
 const dbProfileTo = profile.getByUID(_req.getString("to"));
 const inputMessage = _req.getString("message");
 
-_db.form("message")
+const dbMessageInserted = _db.form("message")
   .set("from_profile_id", dbProfileFrom.getInt("id"))
   .set("to_profile_id", dbProfileTo.getInt("id"))
   .set("message", inputMessage)
   .set("sent_on", _db.timestamp())
   .insert();
 
-profile.wsSendService(
-  dbProfileTo,
-  _val.map()
-    .set("service", "message/unread/count")
-);
+const dbMessage = _db.form("message")
+  .where(_db.where("id").equal(dbMessageInserted.getInt("id")))
+  .first();
 
-profile.wsSendService(
+profile.wsSendAsService(
   dbProfileTo,
   _val.map()
     .set("method", "POST")
-    .set("service", "message/list")
+    .set("service", "message/new")
     .set(
       "data",
       _val.map()
         .set("with", dbProfileFrom.getString("uid"))
+    )
+    .set(
+      "content",
+      message.toData(dbProfileFrom, dbProfileTo, dbMessage)
     )
 );
 
