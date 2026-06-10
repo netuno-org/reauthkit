18db4e18625dd95443fda88fd17e6d60638c2e09
Eduardo Velasques <eduveks@gmail.com>
2026-06-10 21:03:25 +0100

    New service for marking a message as read.

---
+++ server/services/message/read/mark/get.js
@@ -0,0 +1,20 @@
+import {_req, _db, _out, _val} from "@netuno/server-types";
+
+import profile from "#core/lib/profile.js";
+import message from "#core/lib/message.js";
+
+const dbProfileFrom = profile.getByUID(_req.getString("from"));
+
+const dbMessage = message.getByUID(_req.getString("uid"));
+
+_db.execute(`
+  UPDATE message SET read_on = CURRENT_TIMESTAMP
+  WHERE read_on IS NULL AND id = ?::int AND from_profile_id = ?::int
+`, dbMessage.getInt("id"), dbProfileFrom.getInt("id"));
+
+_out.json(
+  _val.map()
+    .set("result", true)
+    .set("from", dbProfileFrom.getString("uid"))
+);
+
