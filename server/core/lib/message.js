6c13d7c0dc52ab9fc8fc3d4b6f4f6290f374899d
Eduardo Velasques <eduveks@gmail.com>
2026-06-10 20:59:29 +0100

    Message lib with more useful methods implemented.

--- server/core/lib/message.js
+++ server/core/lib/message.js
@@ -1,10 +1,24 @@
-import {_db} from "@netuno/server-types";
+import {_db, _val} from "@netuno/server-types";
 
 export default {
+  getByUID: (uid) => {
+    return _db.form("message")
+      .where(_db.where("uid").equal(uid))
+      .first();
+  },
   getUnreadTotal: (dbPeople) => {
     const dbMessagesUnread = _db.queryFirst(`
       SELECT COUNT(id) AS total FROM message WHERE to_profile_id = ? AND read_on IS NULL
     `, dbPeople.getInt("id"))
     return dbMessagesUnread.getInt("total", 0)
+  },
+  toData: (dbProfileFrom, dbProfileTo, dbMessage) => {
+    return _val.map()
+      .set("uid", dbMessage.getString("uid"))
+      .set("from", dbProfileFrom.getString("uid"))
+      .set("to", dbProfileTo.getString("uid"))
+      .set("message", dbMessage.getString("message"))
+      .set("sent_on", dbMessage.getSQLTimestamp("sent_on"))
+      .set("read_on", dbMessage.getSQLTimestamp("read_on"));
   }
 }
