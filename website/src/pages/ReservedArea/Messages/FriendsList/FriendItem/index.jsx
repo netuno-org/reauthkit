671af168dec65647b2592ae2e04b1a1505229042
Eduardo Velasques <eduveks@gmail.com>
2026-06-11 22:23:47 +0100

    Friends list item shows unread messages and the last message time.

--- website/src/pages/ReservedArea/Messages/FriendsList/FriendItem/index.jsx
+++ website/src/pages/ReservedArea/Messages/FriendsList/FriendItem/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {Avatar, Badge, Col, Row} from "antd";
+import dayjs from "dayjs";
 import _service from "@netuno/service-client";
 
 import "./index.less";
 
-function FriendItem({ uid, name, avatar, online, onClick }) {
+function FriendItem({ uid, name, avatar, online, latest_message, unread_messages, onClick }) {
     return (
         <li onClick={onClick} className="messages__friends-list__item">
             <Row align="middle">
@@ -18,6 +19,12 @@ function FriendItem({ uid, name, avatar, online, onClick }) {
                 </Col>
                 <Col flex="auto" className="messages__friends-list__item__name">
                     {name}
+                </Col>
+                <Col flex="50px" className="messages__friends-list__item__messages">
+                  {unread_messages > 0 && <>
+                    <div className="messages__friends-list__item__messages__latest">{dayjs(latest_message).fromNow(true)}</div>
+                    <Badge color="green" count={unread_messages} />
+                  </>}
                 </Col>
             </Row>
         </li>
