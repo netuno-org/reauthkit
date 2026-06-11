f6f05c594c2e782640732794597c833b5668a541
Eduardo Velasques <eduveks@gmail.com>
2026-06-11 22:26:39 +0100

    Chat history component was refactored with the message read mark protocol.

--- website/src/pages/ReservedArea/Messages/Chat/History/index.jsx
+++ website/src/pages/ReservedArea/Messages/Chat/History/index.jsx
@@ -7,53 +7,82 @@ import _ws from "@netuno/ws-client";
 import Message from "./Message/index.jsx";
 
 import "./index.less";
+import globalNotification from "../../../../../common/globalNotification.js";
 
 function History({friend, reload}) {
-    const [loading, setLoading] = useState(true);
-    const [messages, setMessages] = useState([]);
-    const refList = useRef(null);
-    useEffect(() => {
-        const listenerMessageRef = _ws.addListener({
-            method: "POST",
-            service: "message/list",
-            success: (data) => {
-                setLoading(false);
-                setMessages(data.content);
-            },
-            fail: (error)=> {
-                setLoading(false);
-            }
+  const [loading, setLoading] = useState(true);
+  const [messages, setMessages] = useState([]);
+  const refList = useRef(null);
+  useEffect(() => {
+    const listenerMessageRef = _ws.addListener({
+      method: "POST",
+      service: "message/list",
+      start: ()=> {
+        setLoading(true);
+      },
+      success: ({content}) => {
+        setMessages(content);
+      },
+      fail: (error) => {
+        console.error(error);
+        globalNotification.serviceFail({
+          title: "Histórico de Mensagens",
+          description: "Houve uma falha ao tentar atualizar o histórico de mensagens.",
         });
-        onLoad();
-        return () => {
-            _ws.removeListener(listenerMessageRef);
+      },
+      end: ()=> {
+        setLoading(false);
+      }
+    });
+    const listenerNewMessageRef = _ws.addListener({
+      method: "POST",
+      service: "message/new",
+      success: ({data, content}) => {
+        if (data.with === friend.uid) {
+          _ws.sendService({
+            service: "message/read/mark",
+            data: {
+              uid: content.uid,
+              from: friend.uid
+            },
+            success: () => {
+              setMessages((prev) => [...prev, content]);
+            }
+          });
         }
-    }, [friend]);
+      }
+    });
+    onLoad();
+    return () => {
+      _ws.removeListener(listenerMessageRef);
+      _ws.removeListener(listenerNewMessageRef);
+    }
+  }, [friend]);
   useEffect(() => {
     refList.current.scrollTo({top: refList.current.scrollHeight});
   }, [messages]);
-    useEffect(() => {
-        if (reload > 0) {
-            onLoad();
-        }
-    }, [reload]);
-    const onLoad = () => {
-        _ws.sendService({
-            method: "POST",
-            service: "message/list",
-            data: {
-                with: friend.uid
-            }
-        });
-    };
-    return (
-        <ul className="messages__chat__history" ref={refList}>
-            {loading && <li><Spin /></li>}
-            { messages.map((message) => (
-                <Message key={message.uid} friend={friend} data={message} />
-            )) }
-        </ul>
-    );
+  useEffect(() => {
+    if (reload > 0) {
+      onLoad();
+    }
+  }, [reload]);
+  const onLoad = () => {
+    _ws.sendService({
+      method: "POST",
+      service: "message/list",
+      data: {
+        with: friend.uid
+      }
+    });
+  };
+  return (
+    <ul className="messages__chat__history" ref={refList}>
+      {loading && <li><Spin/></li>}
+      {messages.map((message) => (
+        <Message key={message.uid} friend={friend} data={message}/>
+      ))}
+    </ul>
+  );
 }
 
 export default History;
