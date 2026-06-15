79a56d313a0db010bdc2216196e272d41799fbb3
Eduardo Velasques <eduveks@gmail.com>
2026-06-15 11:06:47 +0100

    Default group name and code were updated.

--- server/setup/000-group.js
+++ server/setup/000-group.js
@@ -2,6 +2,6 @@ import {_group, _val} from '@netuno/server-types'
 
 _group.createIfNotExists(
     _val.map()
-        .set("name", "Pessoa")
-        .set("code", "people")
+        .set("name", "Genérico")
+        .set("code", "generic")
 )
