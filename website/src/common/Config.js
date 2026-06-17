a86a327050242b68e29bd420066800d893ad44d6
Eduardo Velasques <eduveks@gmail.com>
2026-06-17 14:13:03 +0100

    Website configuration supports Push Notifications.

--- website/src/common/Config.js
+++ website/src/common/Config.js
@@ -1,11 +1,15 @@
 import _service from '@netuno/service-client';
 import {notification} from 'antd';
+import _auth from "@netuno/auth-client";
 
 class Config {
   static init() {
     const { config } = window.reauthkit;
     _service.config({
       prefix: config.services.prefix
+    });
+    _auth.config({
+      storage: 'local'
     });
     notification.config({
       placement: 'topRight',
@@ -33,6 +37,44 @@ class Config {
   static authAltcha() {
     const { config } = window.reauthkit;
     return !!config.auth.altcha;
+  }
+
+  static push() {
+    const { config } = window.reauthkit;
+    if ("serviceWorker" in navigator) {
+      send().catch(err => console.error(err));
+    }
+
+    async function send() {
+      navigator.serviceWorker.register("/push-worker.js", {
+        scope: "/"
+      }).then((registration) => {
+        registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlBase64ToUint8Array(config.push.key)
+        }).then((subscription) => {
+          _service({
+            method: "POST",
+            url: "notification/subscribe",
+            data: subscription,
+          })
+        });
+      });
+    }
+
+    function urlBase64ToUint8Array(base64String) {
+      const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+      const base64 = (base64String + padding)
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    }
   }
 }
 
